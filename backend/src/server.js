0dad3204fb09b6fcbce04f8d7f01599d517843df
faresnedal1812 <nedalfares53@gmail.com>
2026-02-14 21:07:46 +0200

    database setup added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,23 @@
 import express from "express";
 import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 
-app.listen(5001, () =>
-  console.log("Server is up and running on port:", ENV.PORT),
-);
+app.get("/", (req, res) => {
+  res.send("Hello from server!");
+});
+
+function startServer() {
+  try {
+    connectDB();
+    app.listen(ENV.PORT, () =>
+      console.log("Server is up and running on port:", ENV.PORT),
+    );
+  } catch (error) {
+    console.log("💣 Error starting the server:", error.message);
+    process.exit(1);
+  }
+}
+
+startServer();
