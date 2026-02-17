d22786e186a1e81655f7bd8bbf987792f396bc70
faresnedal1812 <nedalfares53@gmail.com>
2026-02-17 17:47:10 +0200

    notification routes added

---
+++ backend/src/controllers/notification.controller.js
@@ -0,0 +1,32 @@
+import asyncHandeler from "express-async-handler";
+import { getAuth } from "@clerk/express";
+import Notification from "../models/notification.model.js";
+import User from "../models/user.model.js";
+
+export const getNotifications = asyncHandeler(async (req, res) => {
+  const notifications = await Notification.find({})
+    .sort({ ceratedAt: -1 })
+    .populate("user", "username firstName lastName profilePicture")
+    .populate("post", "content image")
+    .populate("comment", "content");
+
+  res.status(200).json({ notifications });
+});
+
+export const deleteNotification = asyncHandeler(async (req, res) => {
+  const { notificationId } = req.params;
+  const { userId } = getAuth(req);
+
+  const user = await User.findOne({ clerkId: userId });
+  if (!user) return res.status(404).json({ error: "User not found" });
+
+  const notification = await Notification.findOneAndDelete({
+    _id: notificationId,
+    to: user._id,
+  });
+
+  if (!notification)
+    return res.status(404).json({ error: "Notification not found" });
+
+  res.status(200).json({ message: "Notification deleted successfully" });
+});
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import { clerkMiddleware } from "@clerk/express";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
+import notificationRoutes from "./routes/notification.route.js";
 
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
@@ -19,9 +20,10 @@ app.get("/", (req, res) => {
   res.send("Hello from server!");
 });
 
-app.use("/api/user", userRoutes);
+app.use("/api/userس", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 app.use((err, req, res, next) => {
   console.log("Unhandled error:", err);
