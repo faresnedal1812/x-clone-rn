305ce3d26e6b09baf1c3dcf073df94c45fcd0a4f
faresnedal1812 <nedalfares53@gmail.com>
2026-03-06 19:28:32 +0200

    like and delete post functionality added

---
+++ mobile/utils/formatters.ts
@@ -0,0 +1,28 @@
+import {
+  differenceInMinutes,
+  differenceInHours,
+  differenceInDays,
+} from "date-fns";
+
+export const formatNumber = (num: number): string => {
+  if (num > 1000) return Math.floor(num) + "K";
+  return num.toString();
+};
+
+export const formatDate = (dateString: string): string => {
+  const date = new Date(dateString);
+  if (isNaN(date.getTime())) {
+    return "Invalid Date";
+  }
+  const now = new Date();
+
+  const minutes = differenceInMinutes(now, date);
+  const houres = differenceInHours(now, date);
+  const days = differenceInDays(now, date);
+
+  if (minutes < 1) return `now`;
+  if (minutes < 60) return `${minutes}m`;
+  if (houres < 24) return `${houres}h`;
+  if (days < 7) return `${days}d`;
+  return Math.floor(days / 7) + "w";
+};
